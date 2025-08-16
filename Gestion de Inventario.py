49118890324ace875e88b22ce3cf2fc5bd8a2a98
Proyectos de Pycharm <git@github.com:Aguilar-Grecia/PycharmProjects.git>
2025-08-15 23:15:38 -0600

    fucnion de listar productos

--- Gestion de Inventario.py
+++ Gestion de Inventario.py
@@ -19,6 +19,16 @@ class Ordenador:
             return lista
 
         menores = [x for x in lista[1:] if obtener_valor(x) < pivote]
-        oguales = [x for x in lista if obtener_valor(x) == pivote]
+        iguales = [x for x in lista if obtener_valor(x) == pivote]
         mayores = [x for x in lista [1:] if obtener_valor(x) > pivote]
         return Ordenador.quick_sort(menores, clave) + iguales + Ordenador.quick_sort(mayores, clave)
+
+    #la funcion listar productos iria adentro de la clase inventario ya que es allí donde se van a mostrar los productos
+    def listar_productos(self, clave_orden = "nombre"):
+        lista = list(self.productos.values())
+        if not lista:
+            print("No hay productos en el invenario para mostra.")
+            return
+        lista_ordenada = Ordenador.quick_sort(lista, clave_orden)
+        for p in lista_ordenada:
+            print(f"{p.codigo} - {p.nombre} - {p.categoria} - Q{p.precio} - Stock: {p.stock}")
