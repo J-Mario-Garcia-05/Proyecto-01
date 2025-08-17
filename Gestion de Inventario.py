bd9cde9105bf7d66b61368ed98bc04f9eb3cbaf3
Proyectos de Pycharm <git@github.com:Aguilar-Grecia/PycharmProjects.git>
2025-08-17 16:36:53 -0600

    agregando menu

--- Gestion de Inventario.py
+++ Gestion de Inventario.py
@@ -3,21 +3,6 @@ class Ordenador:
         if len(lista) <= 1:
             return lista
 
-    def obtener_valor(producto):
-        if clave == "nombre":
-            return producto.nombre
-        elif clave == "precio":
-            return producto.precio
-        elif clave == "stock":
-            return producto.stock
-        else:
-            return None
-
-        pivote = obtener_valor(lista[0])
-        if pivote is None:
-            print("Error: la clave del orden inválido.")
-            return lista
-
         menores = [x for x in lista[1:] if obtener_valor(x) < pivote]
         iguales = [x for x in lista if obtener_valor(x) == pivote]
         mayores = [x for x in lista [1:] if obtener_valor(x) > pivote]
@@ -34,12 +19,60 @@ class Buscador:
             elif clave == "categoria" and valor in producto.categoria:
                 resultados.append(producto)
         return resultados
-    #la funcion listar productos iria adentro de la clase inventario ya que es allí donde se van a mostrar los productos
-    def listar_productos(self, clave_orden = "nombre"):
+
+        def buscar_productos(lista,clave,valor):
+            lista = list(self.productos.values())
+            if not lista:
+                print("No hay productos en el inventario. ")
+                return
+
+            resultado = Buscador.buscar(lista,clave,valor)
+            if resultados:
+                for p in resultados:
+                    print(f"{p.codgio} - {p.nombre} - {p.categoria} - Q{p.precio} - {p.stock}")
+            else:
+                print("No se encontraron productos. ")
+
+    #la funcion listar productos iria adentro de la clase inventario ya que es allí donde se van a mostrar los productos hice la funcion de agregar producto para ver lo de la funcion listar producto
+
+    def agregar_producto(self,producto):
+            if producto.codigo in self. productos:
+                print("El código ya existe dentro del inventario. ")
+            else:
+                self.productos[producto.codigo] = producto
+
+    def listar_productos(self, clave="nombre"):
         lista = list(self.productos.values())
         if not lista:
-            print("No hay productos en el invenario para mostra.")
+            print("No hay productos en el invenario para mostrar.")
             return
         lista_ordenada = Ordenador.quick_sort(lista, clave_orden)
         for p in lista_ordenada:
             print(f"{p.codigo} - {p.nombre} - {p.categoria} - Q{p.precio} - Stock: {p.stock}")
+
+#MENU
+def menu():
+    invenario = Inventario()
+
+    while True:
+        print("\n---MENÚ DE INVENTARIO---")
+        print("1. Agregar producto")
+        print("2. Listar productos")
+        print("3. Buscar producto")
+        print("4. Salir")
+        opcion = input("Seleccione una opción:")
+
+        if opcion == "1":
+            codigo = input("Código del producto: ")
+            nombre = input("Nombre del producto: ")
+            categoria = input("Categoria del producto: ")
+            try:
+                precio = float(input("Precio del producto: "))
+                stock = int(input("Stock del producto: "))
+            except ValueError:
+                print("Precio o stock invalido. Intentelo de nuevo.")
+                continue
+            producto = Productos(codigo, nombre, categoria, precio, stock)
+            inventario.agregar_producto(producto)
+
+        elif opcion == "2":
