485aaf3ae1f33989296f9e670a1de4073926cbdc
J-Mario-García <jm.gcaxaj@udeo.edu.gt>
2025-08-17 18:29:19 -0600

    Modificar clases

--- GestionProducto.py
+++ GestionProducto.py
@@ -1,9 +1,20 @@
 class Producto:
-    def __init__(self, nombre, categoria, precio, stock):
+    def __init__(self, codigo, nombre, categoria, precio, stock):
+        self.codigo = codigo
         self.__nombre = nombre
         self.__categoria = categoria
         self.__precio = precio
         self.__stock = stock
+
+    @property
+    def codigo(self):
+        return self.__codigo
+    @codigo.setter
+    def codigo(self, value):
+        if value != '':
+            self.__codigo = value
+        else:
+            raise ValueError('Codigo no valido')
 
     @property
     def nombre(self):
@@ -11,10 +22,10 @@ class Producto:
 
     @nombre.setter
     def nombre(self, value):
-        if value is not None:
+        if value != '':
             self.__nombre = value
         else:
-            raise ValueError("No puedo ingreasar campos vacíos")
+            raise ValueError("El nombre no puede estar vacío")
 
     @property
     def categoria(self):
@@ -22,10 +33,10 @@ class Producto:
 
     @categoria.setter
     def categoria(self, value):
-        if value is not None:
+        if value != '':
             self.__categoria = value
         else:
-            raise ValueError("No puedo ingreasar campos vacíos")
+            raise ValueError("La categoría no puede estar vacío")
 
     @property
     def precio(self):
@@ -45,54 +56,38 @@ class Producto:
     @stock.setter
     def stock(self, value):
         if value < 0:
-            raise ValueError("El número de stock debe ser mayor a 0")
+            raise ValueError("El stock no puede ser negativo")
 
     def __str__(self):
-        return f'{self.__nombre}, categoría: {self.__categoria}, precio: Q.{self.__precio:.2f}, cantidad en stock: {self.__stock}'
-
+        return f"{self.__codigo} - {self.__nombre}, categoría: {self.__categoria}, precio: Q.{self.__precio:.2f}, stock: {self.__stock}"
 
 class Inventario:
     def __init__(self):
         self.inventario = {}
 
-    def agregar_producto(self):
-        while True:
-            try:
-                codigo = input("Ingrese el codigo del producto: ")
-                if codigo in self.inventario:
-                    raise ValueError("Ya existe un producto con mismo código")
-                nombre = input("\tNombre del producto: ")
-                categoria = input("\tCategoria del producto: ")
-                precio = float(input("\tPrecio del producto: "))
-                stock = int(input("\tStock disponible: "))
-                producto = Producto(nombre, categoria, precio, stock)
-                self.inventario[codigo] = {'producto': producto}
-                print("Producto agregado correctamente")
-            except ValueError as e:
-                print(f"Ha ocurrido un error: {e}")
-            break
+    def agregar_producto(self, producto: Producto):
+        if producto.codigo in self.inventario:
+            raise ValueError('Ya existe un producto con el mismo código')
+        self.inventario[producto.codigo] = producto
 
-    def actualizar_precio(self, producto):
-        try:
-            precio = float(input("\tIngrese el nuevo precio: "))
-            self.inventario[producto].precio = precio
-        except ValueError as e:
-            print("Ha ocurrido un error: ", e)
+    def listar_productos(self, clave="nombre"):
+        lista = list(self.productos.values())
+        return Ordenador.quick_sort(lista, clave) if lista else []
 
-    def actualizar_stock(self, producto):
-        try:
-            stock = int(input("\tIngrese el nuevo stock: "))
-            self.inventario[producto].stock = stock
-        except ValueError as e:
-            print("Ha ocurrido un error: ", e)
-
-    def eliminar_producto(self, producto):
-        del self.inventario[producto]
-
-    def mostrar(self, productos):
-        if not self.inventario:
-            print("No hay productos en el inventario")
+    def actualizar_precio(self, codigo, nuevo_precio):
+        if codigo in self.inventario:
+            self.inventario[codigo].precio = nuevo_precio
         else:
-            for clave, producto in productos.items():
-                print("Clave del pproducto: ", clave)
-                print(producto)
+            raise ValueError("Producto no encontrado")
+
+    def actualizar_stock(self, codigo, nuevo_stock):
+        if codigo in self.inventario:
+            self.inventario[codigo].stock = nuevo_stock
+        else:
+            raise ValueError("Producto no encontrado")
+
+    def eliminar_producto(self, codigo):
+        if codigo in self.inventario:
+            del self.inventario[codigo]
+        else:
+            raise ValueError("Producto no encontrado")
